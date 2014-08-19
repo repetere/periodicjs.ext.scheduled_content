e4a24574faae627e9d9d1e938c4c73cf8a487745
Yaw Joseph Etse <yaw.etse@gmail.com>
2014-08-19 10:57:51 -0400

    working on external module

--- index.js
+++ index.js
@@ -1,89 +1,88 @@
+'use strict';
+
 var async = require('async'),
-		CronJob = require('cron').CronJob,
-		appSettings,
-		mongoose,
-		User,
-		Collection,
-		Item,//Post
-		scheduled_itemid_array=[],
-		scheduled_collectionid_array=[],
-		logger;
+    CronJob = require('cron').CronJob,
+    appSettings,
+    mongoose,
+    Collection,
+    Item, //Item
+    scheduled_itemid_array = [],
+    scheduled_collectionid_array = [],
+    logger;
 
-var publishScheduledItemCollectionss = function(){
-    scheduled_collectionid_array=[];
-    scheduled_itemid_array=[];
+var publishScheduledItemCollectionss = function () {
+    scheduled_collectionid_array = [];
+    scheduled_itemid_array = [];
 
-    var updateScheduledContent = function(model,callback){
+    var updateScheduledContent = function (model, callback) {
         model
             .update({
-                status:'schedule',
-                publishat:{
-                    $lt:new Date()
-                }
-            },
-            {
-                status:'publish'
-            },
-            {
-                multi: true
-            },
-            function(err,numberAffected,raw){
-                if(err){
-                    callback(err,null);
-                }
-                else{
-                    if(numberAffected>0){
-                        callback(null,"number of updates items: "+numberAffected+" - "+(new Date()));
+                    status: 'schedule',
+                    publishat: {
+                        $lt: new Date()
                     }
-                    else{
-                        callback(null,null);
+                }, {
+                    status: 'publish'
+                }, {
+                    multi: true
+                },
+                function (err, numberAffected, raw) {
+                    if (err) {
+                        callback(err, null);
+                    }
+                    else {
+                        if (numberAffected > 0) {
+                            callback(null, 'number of updates items: ' + numberAffected + ' - ' + (new Date()),raw);
+                        }
+                        else {
+                            callback(null, null);
+                        }
                     }
                 }
-            }
         );
     };
 
-    try{
+    try {
         var job = new CronJob({
             cronTime: '1 * * * * *',
-            onTick: function() {
+            onTick: function () {
                 async.parallel({
-                    scheduledItems:function(callback){
-                        updateScheduledContent(Item,callback);
-                    },
-                    scheduledCollections:function(callback){
-                        updateScheduledContent(Collection,callback);
-                    }
-                },
-                function(err,results){
-                    if(err){
-                        logger.error(err);
-                    }
-                    else{
-                        if(results.scheduledItems || results.scheduledCollections){
-                            logger.silly(results.scheduledItems);
+                        scheduledItems: function (callback) {
+                            updateScheduledContent(Item, callback);
+                        },
+                        scheduledCollections: function (callback) {
+                            updateScheduledContent(Collection, callback);
                         }
-                    }
-                });
+                    },
+                    function (err, results) {
+                        if (err) {
+                            logger.error(err);
+                        }
+                        else {
+                            if (results.scheduledItems || results.scheduledCollections) {
+                                logger.silly(results.scheduledItems);
+                            }
+                        }
+                    });
             },
-            onComplete:function(){
-            },
+            onComplete: function () {},
             start: true
             // timeZone: "America/Los_Angeles"
         });
+        logger.silly(job);
         // job.start();
     }
-    catch(e){
+    catch (e) {
         logger.error(e);
     }
 };
 
-module.exports = function(periodic){
-	// express,app,logger,config/settings,db
-	logger = periodic.logger;
-	mongoose = periodic.mongoose;
-	appSettings = periodic.settings;
-	Item = mongoose.model('Post');
-	Collection = mongoose.model('Collection');
-	publishScheduledItemCollectionss();
+module.exports = function (periodic) {
+    // express,app,logger,config/settings,db
+    logger = periodic.logger;
+    mongoose = periodic.mongoose;
+    appSettings = periodic.settings;
+    Item = mongoose.model('Item');
+    Collection = mongoose.model('Collection');
+    publishScheduledItemCollectionss();
 };
